2b05a38f46614d2ac2d64ea4d34e9611b0a36559
Kandarp S. Khandwala <kandarp@cse.iitb.ac.in>
2015-01-30 14:55:17 +0530

    lab 2: q2 progress

--- 2/q2.c
+++ 2/q2.c
@@ -1,26 +1,25 @@
 #include <mcs51/at89x52.h>
 
-void timerDelay(void);
-void mask(void);
+void delay(int);
+void process(int);
+int mask(int);
 
 void main(void) {
-    int count = 0x00;
-    P1 = 0x00;
-    P3 = 0b1110;
-    while (1) {
-        if (!P2_0) {
-            while (!P2_0);
-            count = count + 0x01;
-            timerDelay();
-            
-        }
-    }
+	int count = 0x00;
+	while(1) {
+		if(!P2_0) {
+			while(!P2_0);
+			count = count + 0x01;
+			delay(0x35);
+			process(count);
+		}
+	}
 }
 
-void timerDelay(void) {
+void delay(int l) {
 	TMOD = 0x01;
 	TL0 = 0x00;
-	TH0 = 0x35;
+	TH0 = l;
 	TR0 = 1;
 	while (TF0 == 0);
 	TR0 = 0;
@@ -30,32 +29,44 @@ void timerDelay(void) {
 void process(int count) {
 	int tens = count / 10;
 	int units = count % 10;
-
-	mask(units);
-	mask(tens);
+	P3 = 0b1110;
+	P1 = mask(units);
+	delay(0x1F4);
+	P3 = 0b1101;
+	P1 = mask(tens);
 }
 
 int mask(int digit) {
 	switch(digit) {
-	case 0:
-		break;
-	case 1:
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		break;
-	case 7:
-		break;
-	case 8:
-		break;
-	case 9:
-		break;
+		case 0:
+			return 0b11111100;
+			break;
+		case 1:
+			return 0b01100000;
+			break;
+		case 2:
+			return 0b11011010;
+			break;
+		case 3:
+			return 0b11110011;
+			break;
+		case 4:
+			return 0b01100110;
+			break;
+		case 5:
+			return 0b10110110;
+			break;
+		case 6:
+			return 0b10111110;
+			break;
+		case 7:
+			return 0b11100000;
+			break;
+		case 8:
+			return 0b11111110;
+			break;
+		case 9:
+			return 0b11100110;
+			break;
 	}
 }
